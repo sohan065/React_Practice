2e535788d6b4757f1d1430106987afb541613111
unknown <mdsohan065@gmail.com>
2023-06-08 09:00:56 +0600

    reducer updated

--- src/components/STATE_LIFTING/Home.js
+++ src/components/STATE_LIFTING/Home.js
@@ -3,11 +3,7 @@ import Todos from './Todos';
 import NewTodo from './NewTodo';
 import UseEffect from '../UseEffect/UseEffect';
 import Data from '../UseFetch/Data';
-<<<<<<< HEAD
-import FormikForm from '../Formik/FormikForm';
-=======
 import Reduser from '../UseReduser/Reduser';
->>>>>>> c962397b62741103f3abdbb05b3cc49d7acaefb3
 
 export default function Home() {
     const dummyTodos = ['todo1', 'todo2', 'todo3'];
@@ -21,11 +17,7 @@ export default function Home() {
             <Todos todos={todos} /> */}
             {/* <UseEffect /> */}
             {/* <Data /> */}
-<<<<<<< HEAD
-            <FormikForm />
-=======
             <Reduser />
->>>>>>> c962397b62741103f3abdbb05b3cc49d7acaefb3
         </div>
     );
 }
--- src/components/UseReduser/Reduser.js
+++ src/components/UseReduser/Reduser.js
@@ -1,31 +1,58 @@
-import { Form } from 'formik';
-import React, { useState } from 'react';
+/* eslint-disable max-lines */
+import React, { useReducer, useState } from 'react';
 
+const bookList = [
+    { id: 1, name: 'java' },
+    { id: 2, name: 'Php' },
+    { id: 3, name: 'Asp' }
+];
 export default function Reduser() {
     const [bookName, setBookName] = useState('');
-    const [isModalOpen, setModalOpen] = useState(false);
-    const [modalText, setModalText] = useState('');
-    const bookList = [
-        { id: 1, name: 'java' },
-        { id: 2, name: 'Php' },
-        { id: 3, name: 'Asp' }
-    ];
-    const [books, setBooks] = useState(bookList);
+    // const [isModalOpen, setModalOpen] = useState(false);
+    // const [modalText, setModalText] = useState('');
+    // const [books, setBooks] = useState(bookList);
+
+    const reducer = (state, action) => {
+        if (action.type === 'ADD') {
+            const allBoooks = [...state.books, action.payload];
+            return {
+                ...state,
+                books: allBoooks,
+                isModalOpen: true,
+                modalText: 'new book added'
+            };
+        }
+        if (action.type === 'REMOVE') {
+            console.log(action.payload);
+            const filterBoooks = [...state.books].filter((book) => book.id !== action.payload);
+            return {
+                ...state,
+                books: filterBoooks,
+                isModalOpen: true,
+                modalText: 'book removed'
+            };
+        }
+    };
+    const [bookState, dispatch] = useReducer(reducer, {
+        books: bookList,
+        isModalOpen: false,
+        modalText: 'new book added'
+    });
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        setBooks((books) => {
-            const newBook = {
-                id: new Date().getTime().toString(),
-                name: bookName
-            };
-            return [...books, newBook];
-        });
-        setModalOpen(true);
-        setModalText('new book added');
+        const newBook = {
+            id: new Date().getTime().toString(),
+            name: bookName
+        };
+        dispatch({ type: 'ADD', payload: newBook });
+        setBookName('');
     };
     const Modal = ({ modalText }) => {
         return <p>{modalText}</p>;
+    };
+    const removeBook = (id) => {
+        dispatch({ type: 'REMOVE', payload: { id } });
     };
     return (
         <div>
@@ -40,10 +67,20 @@ export default function Reduser() {
                 />
                 <button type="submit">submit</button>
             </form>
-            {isModalOpen && <Modal modalText={modalText} />}
-            {books.map((book) => {
+            {bookState.isModalOpen && <Modal modalText={bookState.modalText} />}
+            {bookState.books.map((book) => {
                 const { id, name } = book;
-                return <li key={id}>{name}</li>;
+                return (
+                    <li key={id}>
+                        {name}
+                        <button
+                            onClick={() => {
+                                removeBook(id);
+                            }}>
+                            remove
+                        </button>
+                    </li>
+                );
             })}
         </div>
     );
