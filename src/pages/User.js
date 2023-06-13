304f51036d95d3f3a32aa1c62c7b89725231d4fd
unknown <mdsohan065@gmail.com>
2023-06-13 08:40:55 +0600

    query parameter added

--- src/pages/User.js
+++ src/pages/User.js
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSearchParams } from 'react-router-dom';
 
 export default function User() {
     const [searchParam, setSearchParam] = useSearchParams();
 
-    return <div>{searchParam.get('id')}</div>;
+    const [name, setName] = useState('');
+    const [age, setAge] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setSearchParam({ name: name, age: age });
+    };
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+                <input type="text" value={age} onChange={(e) => setAge(e.target.value)} />
+                <button type="submit">find </button>
+            </form>
+
+            {/* {searchParam.get('id')} */}
+        </div>
+    );
 }
